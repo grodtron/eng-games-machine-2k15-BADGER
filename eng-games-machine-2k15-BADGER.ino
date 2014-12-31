b1e89eb60f4706af13ba2fa9eddf9c1599eb2979
Gordon <gb@gordonbailey.net>
2014-12-31 07:28:47 -0500

    Oli in progress stuff with motors I think

--- eng-games-machine-2k15-BADGER.ino
+++ eng-games-machine-2k15-BADGER.ino
@@ -4,29 +4,31 @@
 - Measure hole & wood period every shot, compare it to some expected value?
       Figure out when is the best time to calculate it too
 - Need to find a way to measure accurately the shooting time of the target
-- Explore other motor speed for optimal trajectory (needs a better shooting success rate
+- Explore other motor speed for optimal trajectory (needs a better shooting success rate)
 x Opening servos slower when releasing flaps for shooting (TO BE TESTED)
 
 */
 #include <Adafruit_PWMServoDriver.h>
 #include <Adafruit_TCS34725.h>
-#include <Adafruit_INA219.h>
 #include <Wire.h>
-#include "SingleMotorTweener.h"
-//#include "motors.h"
 
 #define BADGER_ADDRESS 0x20
 #define LOADER_ADDRESS 0x21
-#define BATTERY_THRESHOLD 9.5
+
+const int N_MOTORS = 4;
+
+/*const int eStopPin = 7;
+const int leftMotorErrorPin = 1;
+const int rightMotorErrorPin = 0;*/
 
 const int tiltSensePin = 13;
 const int tiltSensePin2 = A2;
 const int rightSensePin = 4;
 const int leftSensePin = 2;
+const int topLeftSensePin = 3;
 const int leftBeltSensePin = A0;
 const int rightBeltSensePin = A3;
 //const int targetPin = 3;
-const int startButton = 3;
 const int irSensorPin = A1;      
 
 // TODO
@@ -36,18 +38,49 @@ const int ledPinC   = 12;
 
 Adafruit_PWMServoDriver servos = Adafruit_PWMServoDriver();
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_2_4MS, TCS34725_GAIN_1X);
-Adafruit_INA219 ina219;
 
 #define CLOSE 510
 #define OPEN 215
 #define MAX_BAG_COUNT 8
-const int servoOrder[MAX_BAG_COUNT] = {1, 5, 2, 4, 3, 7, 12, 6}; // Order to close and open flaps, alternating each tower
+const int servoOrder[MAX_BAG_COUNT] = {1, 5, 2, 4, 3, 7, 0, 6}; // Order to close and open flaps, alternating each tower
 
-const int pwmPins [N_MOTORS] = {9,  10, 11, 6};
-const int digPins [N_MOTORS] = {12, 5, 8, 7};
+int pwmPins [N_MOTORS] = {9,  10, 11, 6};
+int digPins [N_MOTORS] = {12, 5, 8, 7};
+
+const int RIGHT_FRONT_PWM  = 9;
+const int RIGHT_REAR_PWM  = 11;
+const int LEFT_FRONT_PWM = 10;
+const int LEFT_REAR_PWM = 6;
+
+const int RIGHT_FRONT_DIR  = 12;
+const int RIGHT_REAR_DIR  = 8;
+const int LEFT_FRONT_DIR = 5;
+const int LEFT_REAR_DIR = 7;
+                
 int bag_count = 0;
-int total_bags = MAX_BAG_COUNT;
-boolean left, topLeft, right, tilt, tilt2, start, started = false;
+
+#define MOVE_WHEEL_FWD(SIDE, FRONT_REAR, AMOUNT) do { \
+  analogWrite (SIDE ## _ ## FRONT_REAR ## _PWM, 255 - (AMOUNT)); \
+  digitalWrite(SIDE ## _ ## FRONT_REAR ## _DIR, HIGH); \
+} while(0)
+
+#define MOVE_WHEEL_BAK(SIDE, FRONT_REAR, AMOUNT) do { \
+  analogWrite (SIDE ## _ ## FRONT_REAR ## _PWM, (AMOUNT)); \
+  digitalWrite(SIDE ## _ ## FRONT_REAR ## _DIR, LOW); \
+} while(0)
+
+
+#define MOVE_SIDE_FWD(SIDE, AMOUNT) do {  \
+  MOVE_WHEEL_FWD(SIDE, FRONT, AMOUNT); \
+  MOVE_WHEEL_FWD(SIDE, REAR, AMOUNT);  \
+} while(0)
+
+#define MOVE_SIDE_BAK(SIDE, AMOUNT) do {          \
+  MOVE_WHEEL_BAK(SIDE, FRONT, AMOUNT); \
+  MOVE_WHEEL_BAK(SIDE, REAR, AMOUNT);  \
+} while(0)
+
+boolean left, topLeft, right, tilt, tilt2;
 void (*currentState)();
 
 void softStarting();
@@ -55,7 +88,7 @@ void onOffStyle(); // Race mode
 void doingTrackOnRightSide(); // Tower mode
 void error(int code);
 
-#define RUN_TESTS 0
+#define RUN_TESTS 1
 
 void setup(){
   Serial.begin(9600);
@@ -74,7 +107,7 @@ void setup(){
 //  MOVE_SIDE_FWD(RIGHT, 0);
 
   pinMode(leftSensePin, INPUT);
-  pinMode(startButton, INPUT);
+  pinMode(topLeftSensePin, INPUT);
   pinMode(rightSensePin, INPUT);
   pinMode(tiltSensePin, INPUT);
   pinMode(tiltSensePin2, INPUT);
@@ -86,16 +119,21 @@ void setup(){
   digitalWrite(tiltSensePin, HIGH);
   digitalWrite(tiltSensePin2, HIGH);
 
-  /*pinMode(ledPinA, OUTPUT);
+  /*
+  pinMode(eStopPin, INPUT);  
+  pinMode(leftMotorErrorPin, INPUT);
+  pinMode(rightMotorErrorPin, INPUT);
+  pinMode(ledPinA, OUTPUT);
   pinMode(ledPinB, OUTPUT);
   pinMode(ledPinC, OUTPUT);*/
 
   servos.begin();
   servos.setPWMFreq(60);
 
-  for(int i = 0; i < 5; ++i) {
+  for(int i = 0; i < 10; ++i) {
     servos.setPWM(9, 0, 4095); // Belt motor off
   }
+  currentState = waitingForBags;
 
   // open all flaps
   for(int i = 2; i < MAX_BAG_COUNT; ++i) {
@@ -107,17 +145,12 @@ void setup(){
   delay(100);
   servos.setPWM(servoOrder[1], 0, CLOSE);
 
-  // TODO: Check for left or right switches to know which mode we're on?
   if (tcs.begin()) {
-    Serial.println("Found color sensor, gonna shoot sum bags");
-    currentState = waitingForBags;
-  } else if (digitalRead(leftSensePin)) {
-    Serial.println("No color sensor, we gon run today");
+    Serial.println("Found sensor");
+  } else {
+    Serial.println("No TCS34725 found ... check your connections");
     while (1);
-    currentState = softStarting;
   }
-  
-  ina219.begin();
 
 #if RUN_TESTS  
   runTests();
@@ -135,10 +168,10 @@ void setup(){
 
 void loop(){
   left  = digitalRead(leftSensePin);
+  topLeft = digitalRead(topLeftSensePin);
   right = digitalRead(rightSensePin);
   tilt  = !digitalRead(tiltSensePin);  
   tilt2 = !digitalRead(tiltSensePin2);
-  start = digitalRead(startButton);  
 
   if(currentState){
     currentState();
---
+++ motor_tweening.ino
@@ -0,0 +1,53 @@
+// IN PROGRESS
+/*
+class MotorTweener {
+  public:
+    static const int DELAY_TIME = 1;
+  
+    MotorTweener() : 
+    current(0), target(0),
+    lastUpdateTime(millis() - DELAY_TIME)
+    {}
+  
+    void setTargetSpeed(int newTarget, int newRightTarget){
+      if(target != newTarget){
+        target  = newLeftTarget;
+        Serial.print("New Target: ");Serial.println(leftTarget);
+      }
+    }
+    
+    void update(){
+      long now = millis();
+      if(now - DELAY_TIME > lastUpdateTime){
+        lastUpdateTime = now;
+        
+        int leftDelta = leftTarget - left;
+        if(leftDelta){
+          left += (leftDelta/2);
+          Serial.print("Update left : "); Serial.println(left);
+        }
+        
+        int rightDelta = rightTarget - right;
+        if(rightDelta){
+          right += (rightDelta/2);
+          Serial.print("Update right: "); Serial.println(right);
+        }
+        
+        if(left > 0){
+          MOVE_SIDE_FWD(LEFT, left);
+        }else{
+          MOVE_SIDE_BAK(LEFT, abs(left));
+        }
+        if(right > 0){
+          MOVE_SIDE_FWD(RIGHT, right);
+        }else{
+          MOVE_SIDE_BAK(RIGHT, abs(right));
+        }
+      }
+    }
+  private:
+    int left,  leftTarget;
+    int right, rightTarget;
+    long lastUpdateTime;
+  
+};*/
