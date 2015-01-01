eb48dd49cacef6628c9c7f1d6fade5d86b0bc2a1
Gordon <gb@gordonbailey.net>
2015-01-01 01:30:05 -0500

    Final working initialization and servo order

--- eng-games-machine-2k15-BADGER.ino
+++ eng-games-machine-2k15-BADGER.ino
@@ -15,7 +15,7 @@ x Opening servos slower when releasing flaps for shooting (TO BE TESTED)
 //#include "motors.h"
 
 #include <Adafruit_INA219.h>
-Adafruit_INA219 ina219;
+Adafruit_INA219 ina219(0x41);
 
 #define BADGER_ADDRESS 0x20
 #define LOADER_ADDRESS 0x21
@@ -34,18 +34,13 @@ const int rightBeltSensePin = A3;
 //const int targetPin = 3;
 const int irSensorPin = A1;      
 
-// TODO
-const int ledPinA   = 12;
-const int ledPinB   = 12;
-const int ledPinC   = 12;
-
 Adafruit_PWMServoDriver servos = Adafruit_PWMServoDriver();
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_2_4MS, TCS34725_GAIN_1X);
 
-#define CLOSE 510
+#define CLOSE 490
 #define OPEN 215
 #define MAX_BAG_COUNT 8
-const int servoOrder[MAX_BAG_COUNT] = {1, 5, 2, 4, 3, 7, 0, 6}; // Order to close and open flaps, alternating each tower
+const int servoOrder[MAX_BAG_COUNT] = {1, 5, 0, 4, 3, 7, 2, 6}; // Order to close and open flaps, alternating each tower
 
 int pwmPins [N_MOTORS] = {9,  10, 11, 6};
 int digPins [N_MOTORS] = {12, 5, 8, 7};
@@ -62,12 +57,16 @@ void onOffStyle(); // Race mode
 void doingTrackOnRightSide(); // Tower mode
 void error(int code);
 
-#define RUN_TESTS 1
+#define RUN_TESTS 0
+
+int freeRam () {
+  extern int __heap_start, *__brkval; 
+  int v; 
+  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
+}
 
 void setup(){
   Serial.begin(9600);
-
-  Serial.println("testing");
   
   Wire.begin(BADGER_ADDRESS);
   Wire.onReceive(receiveEvent);
@@ -75,18 +74,10 @@ void setup(){
   servos.begin();
   servos.setPWMFreq(60);
 
-  Serial.print("Stopping pulley ");
   for(int i = 0; i < 10; ++i) {
     servos.setPWM(9, 0, 4095); // Belt motor off
-    Serial.print(".");
   }
-  Serial.println(" done");
   
-  Serial.println("opening voltage sensor");
-  ina219.begin();
-
-
-  Serial.println("init'ing motors");
   for(int i = 0; i < N_MOTORS; ++i){
     pinMode(pwmPins[i], OUTPUT);
     pinMode(digPins[i], OUTPUT);
@@ -108,8 +99,6 @@ void setup(){
   digitalWrite(tiltSensePin, HIGH);
   digitalWrite(tiltSensePin2, HIGH);
 
-
-  Serial.println("open all flaps");
   // open all flaps
   for(int i = 2; i < MAX_BAG_COUNT; ++i) {
     servos.setPWM(servoOrder[i], 0, OPEN);
@@ -121,14 +110,14 @@ void setup(){
   servos.setPWM(servoOrder[1], 0, CLOSE);
 
   if (tcs.begin()) {
-    Serial.println("Found color sensor, gonna shoot sum bags");
+    Serial.println("color, bags");
     currentState = waitingForBags;
   } else if (digitalRead(leftSensePin)) {
-    Serial.println("No color sensor, we gon run today");
+    Serial.println("nocolor, laps");
     while (1);
   }
 
-   // Blink color sensor LED
+  // Blink color sensor LED
   for(int i = 0 ; i < 4; ++i) {
     servos.setPWM(15, 0, 0 );
     delay(150);
@@ -140,8 +129,15 @@ void setup(){
   runTests();
   while(1);
 #endif
+ 
+  currentState = waitingForBags;
 
-  currentState = goingDownRamp;
+  Serial.println("initialized");
+
+  Serial.print("ram  "); Serial.println(freeRam());
+
+  ina219.begin();
+  Serial.print("vbat "); Serial.print(ina219.getBusVoltage_V());
 
 }
 
@@ -180,18 +176,18 @@ void error(int code){
 
   while(1){
     for(int i = 0; i < 4; ++i){
-      digitalWrite(ledPinA, HIGH);
-      digitalWrite(ledPinB, HIGH);
-      digitalWrite(ledPinC, HIGH);
+      //digitalWrite(ledPinA, HIGH);
+      //digitalWrite(ledPinB, HIGH);
+      //digitalWrite(ledPinC, HIGH);
       delay(125);
-      digitalWrite(ledPinA, LOW);
-      digitalWrite(ledPinB, LOW);
-      digitalWrite(ledPinC, LOW);
+      //digitalWrite(ledPinA, LOW);
+      //digitalWrite(ledPinB, LOW);
+      //digitalWrite(ledPinC, LOW);
       delay(125);
     }
-    digitalWrite(ledPinA, (code >> 0) & 1);
-    digitalWrite(ledPinB, (code >> 1) & 1);
-    digitalWrite(ledPinC, (code >> 2) & 1);
+    //digitalWrite(ledPinA, (code >> 0) & 1);
+    //digitalWrite(ledPinB, (code >> 1) & 1);
+    //digitalWrite(ledPinC, (code >> 2) & 1);
     delay(1000);
   }
 }
